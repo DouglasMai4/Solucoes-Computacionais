40c935ed9da49b03e8898445cc735ffbb8a6bf32
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2024-06-14 23:12:57 -0300

    feat: add database CRUD

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import database.Estoque;
+import database.EstoqueCRUD;
 import screens.StorageCreate;
 import screens.StorageList;
 
@@ -6,13 +6,28 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        StorageList ScreenList = new StorageList();
+        EstoqueCRUD database = new EstoqueCRUD();
 
-        ScreenList.setVisible(true);
+        // Insert a new contact
+//        database.insert("Produto A", 100, 9.90, "kilo", "Descrição do Produto A");
+//        database.update(3, "Produto Z", 12, 99.9, "unidade", "Descrição do Produto Z");
+//        database.delete(4);
+
+        // Retrieve all contacts
+        System.out.println("All Contacts:");
+        List<EstoqueCRUD.Model> rows = database.getAll();
+
+        for (EstoqueCRUD.Model estoque : rows) {
+            System.out.println(estoque.getId() + ", " + estoque.getNome() + ", " + estoque.getQuantidade() + ", " + estoque.getValor() + ", " + estoque.getUni_medida() + ", " + estoque.getDescricao());
+        }
+//        StorageList ScreenList = new StorageList();
+//
+//        ScreenList.setVisible(true);
     }
 }
--- src/database/Connect.java
+++ src/database/Connect.java
@@ -6,11 +6,28 @@ import java.sql.*;
 // docker run --name mysqlbd -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root mysql                                                                           ✔
 
 public class Connect {
-    private static final String url = "jdbc:mysql://localhost:3306/users?useSSL=true&allowPublicKeyRetrieval=true";
+    private static final String url = "jdbc:mysql://localhost:3306/solucoes-computacionais";
     private static final String username = "root";
     private static final String password = "root";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            // Load the JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
         return DriverManager.getConnection(url, username, password);
     }
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = getConnection();
+            System.out.println("Connection established successfully.");
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
